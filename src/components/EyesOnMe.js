a52ec7f23630052a8bd97415202c8b0756965517
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-11-21 20:55:06 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,7 +2,7 @@
 import React from 'react'
 export default class EyesOnMe extends React.Component {
   handleOnBlur = () => {return console.log('Hey! Eyes on me!')}
-  handleOnFocus = () => {return console.log('Good')}
+  handleOnFocus = () => {return console.log('Good!')}
   
   render = () => {
     return <button onBlur = {this.handleOnBlur} onFocus = {this.handleOnFocus}/>
