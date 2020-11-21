b9827e3e465735f4f9820fa5c4ca645926e2982c
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-11-21 20:48:32 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,6 +1,6 @@
 // Code EyesOnMe Component Here
 import React from 'react'
-class EyesOnMe extends React.Component {
+export default class EyesOnMe extends React.Component {
   onBlur = () => {return console.log('Hey! Eyes on me!')}
   onFocus = () => {return console.log('Good')}
   
