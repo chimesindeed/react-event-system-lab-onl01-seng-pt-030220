e0958bd84681cfd8771d939f0b5f4feac155d596
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-11-21 20:53:54 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default class Keypad extends React.Component {
 
   handleKeyUp = () => {
-    return console.log('Entering password . . .')
+    return console.log('Entering password  .  .  .')
   }
   render = () => {
     return <input type="password" onKeyUp={this.handleKeyUp}/>
